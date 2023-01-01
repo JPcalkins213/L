f3cf811827382079c056c0501a0e9cadd7295c54
JPcalkins <jessicacalkins795213@gmail.com>
2023-01-01 10:52:02 -0600

    adding directive and purpose

--- tasks.py
+++ tasks.py
@@ -8,6 +8,16 @@ def joke():
     print(funny)
     L.say(funny)
 
+def prime_directive():
+    directive = "to protect and live with the Noir family"
+    print(directive)
+    L.say(directive)
+
+def purposes():
+    purpose = "To become human"
+    print(purpose)
+    L.say(purpose)
+
 command = ""
 
 while True and "goodbye" not in command:
@@ -17,7 +27,12 @@ while True and "goodbye" not in command:
     #having L tell me a joke
         if "joke" in command:
             joke()
-    
+        
+        if "prime directive" in command:
+            prime_directive()
+
+        if "purpose" in command:
+            purposes()
 
 
 L.say("goodbye, i'm going to sleep")
